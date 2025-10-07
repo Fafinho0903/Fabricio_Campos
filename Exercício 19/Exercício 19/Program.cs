543d05e3c27dfc7e9617d9a02be84fa14df35f18
Fafinho0903 <fabriciocds090310@gmail.com>
2025-10-07 17:08:55 -0300

    Add files via upload

---
+++ Exercício 19/Exercício 19/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Quantos números você quer digitar?");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 3)
+{
+    Console.WriteLine("Erro:é necessario informar pelo menos 3 números!");
+    return;
+}
+double[] numeros = new double[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Digite o {i + 1}º número:");
+    numeros[i] = double.Parse(Console.ReadLine());
+}
+double menor = numeros.Min();
+double maior = numeros.Max();
+double soma = numeros.Sum() - menor - maior;
+double media = soma / (n - 2);
+
+Console.WriteLine($"\n Menor número: {menor}");
+Console.WriteLine($"Maior número:{maior}");
+Console.WriteLine($"Média (excluindo o menor e o maior):{media:f2}");
