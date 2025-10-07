8e5a8e38bf1377fd81dd7e8e0d4cf89b1c36183e
Fafinho0903 <fabriciocds090310@gmail.com>
2025-10-07 16:08:56 -0300

    Add files via upload

---
+++ Exercício 17/Program.cs
@@ -0,0 +1,19 @@
+﻿double n,i,count,valor;
+Console.WriteLine("Digite um valor");
+n = double.Parse(Console.ReadLine());
+i = 1;
+count = 0;
+while (n >= i)
+{
+    Console.WriteLine($"Digite o {i}º numero:");
+    valor = double.Parse(Console.ReadLine());
+    i++;
+        if (valor < 0)
+    {
+
+    count++;
+    
+    }
+
+}
+Console.WriteLine($"{count} valores são negativos"); 
