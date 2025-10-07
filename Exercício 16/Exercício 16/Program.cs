6561fa2f8a4e1f74901dd91c417c903589fd835e
Fafinho0903 <fabriciocds090310@gmail.com>
2025-10-07 16:08:26 -0300

    Add files via upload

---
+++ Exercício 16/Exercício 16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+double n, a, j;
+
+Console.WriteLine("Escreva o número desejado:");
+n = double.Parse(Console.ReadLine());
+
+a = 2;
+j = n - 1;
+
+while (a > 1)
+{
+    n = n * j;
+    j = j - 1;
+
+    if (j == 0)
+    {
+        a =0;
+    }
+    
+
+}
+Console.Clear();
+Console.WriteLine($"O resultado é: {n}");
